fd99fcb2dc0c86c19c5930366ad1686a99c5dd10
Didi <diana.p.bojanova@gmail.com>
2026-02-18 19:28:10 -0800

    checks if notification about sale was already sent for certain game

--- nintendo_checker.py
+++ nintendo_checker.py
@@ -1,8 +1,21 @@
 import requests
+import json
+import os
 from add_games import load_watchlist
 from notifier import send_notification
 
 WATCH_FILE = "watchlist.json"
+SEEN_FILE = "checked.json"
+
+def load_seen():
+    if not os.path.exists(SEEN_FILE):
+        return []
+    with open(SEEN_FILE, "r") as f:
+        return json.load(f)
+
+def save_seen(seen):
+    with open(SEEN_FILE, "w") as f:
+        json.dump(seen, f, indent=2)
 
 def check_nintendo_price(game_name, id):
 	url=f"https://api.ec.nintendo.com/v1/price?country=US&lang=en&ids={id}"
@@ -10,16 +23,25 @@ def check_nintendo_price(game_name, id):
 	result=response.json()
 
 	price = result["prices"][0]
+	seen = load_seen()
 
 	if price["sales_status"]!="onsale":
 		print(f"{game_name} not available for Switch.")
+		if id in seen:
+			seen.remove(id)
+			save_seen(seen)
 		return
 	
 	if "discount_price" in price:
+		if id in seen:
+			print(f"Already notified about {game_name} sale on Switch.")
+			return
 		normal=price["regular_price"]["amount"]
 		sale=price["discount_price"]["amount"]
 		send_notification(f"{game_name} is on sale for Switch: {sale} rather than  {normal}")
 		print(f"{game_name} is on sale for Switch!")
+		seen.append(id)
+		save_seen(seen)
 	else:
 		print(f"Nothing on sale at the moment.")
 
