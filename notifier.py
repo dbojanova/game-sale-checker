1ba49809445f679af6925a577d1b8dece0897f0b
Didi <diana.p.bojanova@gmail.com>
2026-02-18 19:54:35 -0800

    init commit

---
+++ notifier.py
@@ -0,0 +1,14 @@
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_notification(message):
+	url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+	post_data={
+		"chat_id":CHAT_ID,
+		"text":message
+	}
+	requests.post(url,data=post_data)
