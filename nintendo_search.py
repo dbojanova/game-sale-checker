251dbcd2f5f67a2f50e26d77c52016223f78d2af
Didi <diana.p.bojanova@gmail.com>
2026-02-24 16:29:27 -0800

    algolia api was missing a lot of entries, now using blawar database instead

--- nintendo_search.py
+++ nintendo_search.py
@@ -1,35 +1,42 @@
 import requests
 import json
+import os
+import time
 
-ALGOLIA_URL="https://u3b6gr4ua3-dsn.algolia.net/1/indexes/ncom_game_en_us"
-ALGOLIA_APP_ID="U3B6GR4UA3"
-ALGOLIA_API_KEY="c4da8be7fd29f0f5bfa42920b0a99dc7"
+NSUID_FILE = "titledb_cache.json"
+TITLEDB_URL = "https://raw.githubusercontent.com/blawar/titledb/master/US.en.json"
 IGNORE_WORDS = {"of","the","a","an","and","in","to","for"}
 
+def pull_titledb():
+
+    #update database every week
+    if os.path.exists(NSUID_FILE):
+        time_elapsed = time.time() - os.path.getmtime(NSUID_FILE)
+        if time_elapsed > 7 * 24 * 60 * 60:
+            print("Updating database!")
+            os.remove(NSUID_FILE)
+
+    if not os.path.exists(NSUID_FILE):
+        response = requests.get(TITLEDB_URL)
+        with open(NSUID_FILE, "w") as f:
+            json.dump(response.json(), f)
+    with open(NSUID_FILE, "r") as f:
+        return json.load(f)
+
 def search_nintendo_id(query):
+    db = pull_titledb()
     query_words = query.lower().split()
     search_words = [w for w in query_words if w not in IGNORE_WORDS]
-
-    params = {
-        "query": query,
-        "hitsPerPage": 100,
-        "x-algolia-application-id": ALGOLIA_APP_ID,
-        "x-algolia-api-key": ALGOLIA_API_KEY,
-    }
-
-    response = requests.get(ALGOLIA_URL, params=params)
-    result = response.json()
     
     hits = []
 
-    for hit in result["hits"]:
-        if hit["platform"] == "Nintendo Switch" and hit.get("nsuid"):
-            lower_title = hit["title"].lower()
-            if all(word in lower_title for word in search_words):
-                hits.append({"title": hit["title"], "nsuid": hit["nsuid"]})
-        
-        if hits:
-            return hits
+    for nsuid, game in db.items():
+        name = game.get("name", "")
+        if not name:
+            continue
+        lower_name = name.lower()
+        if all(word in lower_name for word in search_words):
+            hits.append({"title": name, "nsuid": nsuid})
     
     # some games aren't in the API - allow manual entry
     if not hits:
